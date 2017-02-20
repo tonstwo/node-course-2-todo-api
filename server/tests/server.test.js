78abc9ac62ec5b19810a79a64785a315833ccc4f
Edward Anthony M. Polangcos <tonstwo@gmail.com>
2017-02-20 17:35:32 +0300

    Add test for PATCH /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -9,7 +9,9 @@ const todos = [{
     text: 'First test todo'
 }, {
     _id: new ObjectID(),
-    text: 'Second test todo'
+    text: 'Second test todo',
+    completed: true,
+    completedAt: 333
 }];
 
 beforeEach((done) => {
@@ -133,6 +135,40 @@ describe('DELETE /todos/:id', () => {
         request(app)
             .delete(`/todos/123`)
             .expect(404)
+            .end(done);
+    });
+});
+
+describe('PATCH /todos/:id', () => {
+    it('should update the todo', (done) => {
+        var id = todos[0]._id.toHexString();
+
+        request(app)
+            .patch(`/todos/${id}`)
+            .send({text: 'First todo UPDATED',completed: true})
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todo.text).toBe('First todo UPDATED');
+                expect(res.body.todo.completed).toBe(true);
+                expect(res.body.todo.completedAt).toBeA('number');
+                // done();
+            })
+            .end(done);
+
+    });
+
+    it('should clear completedAt when todo is not completed', (done) => {
+        var id = todos[1]._id.toHexString();
+
+        request(app)
+            .patch(`/todos/${id}`)
+            .send({completed: false, text: 'YOLOLOLOLO'})
+            .expect(200)
+            .expect((res)=>{
+                expect(res.body.todo.text).toBe('YOLOLOLOLO');
+                expect(res.body.todo.completed).toBe(false);
+                expect(res.body.todo.completedAt).toNotExist();
+            })
             .end(done);
     });
 });
