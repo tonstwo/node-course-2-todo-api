34ad9c20da4898017c9bb7101e4a524146097177
Edward Anthony M. Polangcos <tonstwo@gmail.com>
2017-02-20 14:09:59 +0300

    Fixed port variable references

--- server/server.js
+++ server/server.js
@@ -49,8 +49,8 @@ app.get('/todos/:id', (req, res)=>{
     });
 });
 
-app.listen(PORT, () => {
-    console.log(`Started up at port ${PORT}`);
+app.listen(port, () => {
+    console.log(`Started up at port ${port}`);
 });
 
 module.exports = {app};
