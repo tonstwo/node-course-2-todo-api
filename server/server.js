07d29ded98bc10f2460cd57275ad9eac0cccfe9d
Edward Anthony M. Polangcos <tonstwo@gmail.com>
2017-02-21 17:50:42 +0300

    Add POST /users route and token method

--- server/server.js
+++ server/server.js
@@ -95,6 +95,19 @@ app.patch('/todos/:id', (req, res) => {
     });
 });
 
+app.post('/users', (req, res) => {
+    var body = _.pick(req.body, ['email', 'password']);
+    var user = new User(body);
+
+    user.save().then(()=>{
+        return user.generateAuthToken();
+    }).then((token)=>{
+        res.header('x-auth', token).send(user);
+    }).catch((er)=>{
+        res.status(400).send(er);
+    });
+})
+
 app.listen(port, () => {
     console.log(`Started up at port ${port}`);
 });
