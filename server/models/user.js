439523dbd1646a6fee6391c6b9e095be04580cd5
Edward Anthony M. Polangcos <tonstwo@gmail.com>
2017-02-22 13:44:56 +0300

    Add POST /users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -33,7 +33,10 @@ var UserSchema = new mongoose.Schema({
     }]
 });
 
-UserSchema.methods.toJSON = function () {
+// THIS toJSON method is the one that allows the server to only send/display the specified fields.
+// In this case,
+// _id and email are specified
+UserSchema.methods.toJSON = function () { 
     var user = this;
     var userObject = user.toObject();
 
@@ -74,6 +77,26 @@ UserSchema.statics.findByToken = function (token) {
         'tokens.access': 'auth'
     });
 };
+
+UserSchema.statics.findByCredentials = function (email, password) {
+    var User = this;
+
+    return User.findOne({email}).then((user) => {
+        if (!user) {
+            return Promise.reject();
+        }
+
+        return new Promise((resolve, reject) => {
+            bcrypt.compare(password, user.password, (err, res) => {
+                if (res) {
+                    resolve(user);
+                } else {
+                    reject();
+                }
+            });
+        });
+    });
+}
 
 UserSchema.pre('save', function (next) {
     var user = this;
